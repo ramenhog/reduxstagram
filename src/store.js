30808ed6e23996dfdd27e4eff2f438c194fa2a93
Stephanie Liu <stephofliu@gmail.com>
2018-03-09 16:25:01 -0500

    add redux tools

--- src/store.js
+++ src/store.js
@@ -10,7 +10,11 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 // https://medium.com/@pshrmn/a-little-bit-of-history-f245306f48dd
 // browserHistory keeps track of the full URL
